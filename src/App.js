8dc086edf7e77db8ddb8f3a0c94e6bb2968536c8
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-08 23:21:00 +0100

    initial changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const api = {
+  key: "09af805cead8519fdfc57d3c69b607ba",
+  base: "https://api.openweathermap.org/data/2.5/",
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      HELLO WORLD
     </div>
   );
 }
