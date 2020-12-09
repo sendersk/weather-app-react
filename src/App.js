b610ba26b8dde34a1314b3da9b9f48d18b0abff5
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-09 20:43:12 +0100

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: "09af805cead8519fdfc57d3c69b607ba",
   base: "https://api.openweathermap.org/data/2.5/",
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = event => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const day = new Date();
+  const today = day.toDateString();
+
   return (
-    <div className="app">
-      HELLO WORLD
+    <div className={
+      (typeof weather.main != "undefined") 
+        ? ((weather.main.temp > 16) 
+          ? 'app warm' 
+          : 'app')
+        : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined")? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{today}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°C
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>  
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
